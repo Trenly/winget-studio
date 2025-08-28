033fc2eea2c503f9543fd190d56013c0bfd30467
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2025-08-28 13:38:49 -0700

    Add logging (#50)

--- src/WinGetStudio/Contracts/Services/IAppInfoService.cs
+++ src/WinGetStudio/Contracts/Services/IAppInfoService.cs
@@ -8,5 +8,24 @@ public interface IAppInfoService
     /// <summary>
     /// Gets the localized application name.
     /// </summary>
+    /// <returns>The localized application name.</returns>
     public string GetAppNameLocalized();
+
+    /// <summary>
+    /// Gets the application version.
+    /// </summary>
+    /// <returns>The application version.</returns>
+    public string GetAppVersion();
+
+    /// <summary>
+    /// Gets the application logs folder.
+    /// </summary>
+    /// <returns>The application logs folder.</returns>
+    public string GetAppLogsPath();
+
+    /// <summary>
+    /// Gets the application instance log path.
+    /// </summary>
+    /// <returns>The application instance log path.</returns>
+    public string GetAppInstanceLogPath();
 }
--- src/WinGetStudio/Services/AppInfoService.cs
+++ src/WinGetStudio/Services/AppInfoService.cs
@@ -1,13 +1,21 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Globalization;
+using System.Reflection;
+using Windows.ApplicationModel;
+using Windows.Storage;
 using WinGetStudio.Contracts.Services;
+using WinGetStudio.Extensions;
 using WinGetStudio.Helpers;
 
 namespace WinGetStudio.Services;
 
 internal sealed class AppInfoService : IAppInfoService
 {
+    // Unique log path for each app instance
+    private readonly string _instanceLogPath = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss-fff", CultureInfo.InvariantCulture);
+
     /// <inheritdoc/>
     public string GetAppNameLocalized()
     {
@@ -16,5 +24,34 @@ internal sealed class AppInfoService : IAppInfoService
 #else
         return "AppDisplayNameDev".GetLocalized();
 #endif
+    }
+
+    /// <inheritdoc/>
+    public string GetAppVersion()
+    {
+        Version version;
+        if (RuntimeHelper.IsMSIX)
+        {
+            var packageVersion = Package.Current.Id.Version;
+            version = new(packageVersion.Major, packageVersion.Minor, packageVersion.Build, packageVersion.Revision);
+        }
+        else
+        {
+            version = Assembly.GetExecutingAssembly().GetName().Version!;
+        }
+
+        return $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+    }
+
+    /// <inheritdoc/>
+    public string GetAppLogsPath()
+    {
+        return Path.Combine(ApplicationData.Current.TemporaryFolder.Path, "Logs");
+    }
+
+    /// <inheritdoc/>
+    public string GetAppInstanceLogPath()
+    {
+        return Path.Combine(GetAppLogsPath(), _instanceLogPath);
     }
 }
